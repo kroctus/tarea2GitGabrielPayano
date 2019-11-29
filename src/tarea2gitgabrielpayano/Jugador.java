0ccd58cae61c9cce083f5b5533b406bfd6c92e35
gabriel_payano <gabriel21kun@gmail.com>
2019-11-29 21:39:09 +0100

    Añade clase Jugador

---
+++ src/tarea2gitgabrielpayano/Jugador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2gitgabrielpayano;
+
+/**
+ *
+ * @author kroctus
+ */
+public class Jugador {
+    
+}
